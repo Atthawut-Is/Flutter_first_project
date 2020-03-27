d80a293067f66a8b5d1e252e28b30f4b8a402e56
Woodito <Atthawut-Is@users.noreply.github.com>
2020-03-27 22:53:27 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(title: Text("woodito"), centerTitle: true,backgroundColor: Colors.grey,),
+      body:
+      Center(
+        child: Container(
+          alignment: Alignment.center,
+          width: 400.0,
+          height: 300.0,
+          color: Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  'Woodito',
+                  style: TextStyle(fontSize: 20),
+
+                ),
+              ),
+//              Image(image:NetworkImage('https://steamcdn-a.akamaihd.net/apps/dota2/images/blog/play/dota_heroes.png')),
+              Image(image:AssetImage('image/dota_heroes.png')),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ),
+);
